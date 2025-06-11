3a409cb4146792a8909a7418f3880814be2d93f3
izhar <mizharzerdi@gmail.com>
2025-06-11 12:43:29 +0530

    Separate even and odd numbers

--- src/com/algo/java8/SeparateEvenAndOdd.java
+++ src/com/algo/java8/SeparateEvenAndOdd.java
@@ -1,7 +1,25 @@
 package com.algo.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class SeparateEvenAndOdd {
     public static void main(String[] args) {
-
+        List<Integer> listOfIntegers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
+        Map<Boolean, List<Integer>> evenOddMap = listOfIntegers.stream().collect(Collectors.partitioningBy(i -> i % 2 == 0));
+        Set<Map.Entry<Boolean, List<Integer>>> entrySet = evenOddMap.entrySet();
+        entrySet.forEach(entry -> {
+            System.out.println("===================");
+            if (entry.getKey()) {
+                System.out.println("Even numbers");
+            } else {
+                System.out.println("Odd numbers");
+            }
+            System.out.println("===================");
+            entry.getValue().forEach(System.out::println);
+        });
     }
 }
